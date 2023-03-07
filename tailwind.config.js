15eb6cde64db3268bfe781a34df463a151b1f389
Teja Paturu <likable_chaste_0s@icloud.com>
2023-03-06 22:51:32 -0700

    🎨 STYLE: Navbar logo size.

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,11 +33,11 @@ module.exports = {
       xs: "25px",
       normal: "170px",
       extended: "510px",
-      logo: "72px",
+      logo: "40px",
       "container-normal": "365px",
     },
     maxWidth: {
-      logo: "72px",
+      logo: "40px",
       "container-extended": "705px",
     },
     minHeight: {
@@ -51,12 +51,12 @@ module.exports = {
         sm: "130 px",
         normal: "170 px",
         extended: "510 px",
-        logo: "73 px",
+        logo: "40 px",
       },
       height: {
         sm: "34 px",
         normal: "44 px",
-        logo: "72 px",
+        logo: "40 px",
       },
       spacing: {
         45: "45px",
